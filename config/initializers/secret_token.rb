cf606541b11bcc3e1ef144fc6ecfacf6fab3b6ab
Edmund Mai <edmundmai@gmail.com>
2012-11-13 12:26:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Langexchange::Application.config.secret_token = '0063c475b28dfcf49575ba50856cbd57b012774a7ad9190f4c6df19e2bf9193fee5e2123506296ef44b824420b3a9693a6d8be7a4d7195121e238952b33305f7'
